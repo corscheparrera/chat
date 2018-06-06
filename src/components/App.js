1b2438f87f33debc7ff17932db9acbbf24d39998
Maxime Lussier <m.lussier@live.ca>
2018-06-06 10:24:02 -0400

    chat id is parsed in url

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,48 @@
-import React, {Component} from 'react'
-import Chat from './Chat.js'
-import {Grid, Row, Col} from 'react-bootstrap'
+import { Col, Grid, Row } from "react-bootstrap";
+import React, { Component } from "react";
 
-const chatPath = 'all-chat/chat-V68h7qVsHiWsIyLcx0T0pC2qrfQ2'
+import Chat from "./Chat.js";
+
+const chatPath = "allChat/chatV68h7qVsHiWsIyLcx0T0pC2qrfQ2";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatId: ""
+    };
+  }
+
+  componentWillMount() {
+    this.getUserId();
+  }
+
+  getUserId = () => {
+    const url = window.location.href;
+    const id = url.replace("http://localhost:3000/", "");
+    this.setState({ chatId: id });
+  };
+
   render() {
     return (
-      <Grid >
+      <Grid>
         <Row>
-
-          <h1 style={{
-            textAlign: "center",
-            paddingBottom: 30,
-          }}>Chat Room
+          <h1
+            style={{
+              textAlign: "center",
+              paddingBottom: 30
+            }}
+          >
+            Chat Room
           </h1>
-
         </Row>
         <Row>
-          <Col sm={12} >
-            <Chat path={chatPath}/>
+          <Col sm={12}>
+            <Chat path={this.state.chatId} />
           </Col>
         </Row>
       </Grid>
-    )
+    );
   }
 }
-export default App
+export default App;
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,68 +1,63 @@
-import React, {Component} from 'react'
-import fire from './Firebase.js'
-import firebase from 'firebase'
-import {Grid, Row, Col} from 'react-bootstrap'
+import { Col, Grid, Row } from "react-bootstrap";
+import React, { Component } from "react";
 
-import Messages from './Messages'
-import ChatInput from './ChatInput'
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+import fire from "./Firebase.js";
+import firebase from "firebase";
 
-const database = fire.database()
+const database = fire.database();
 const avocat = {
-  name: 'Maître Harvey'
-}
+  name: "Maître Harvey"
+};
 
 export default class Chat extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      chat: []
-    }
+      chat: [],
+      path: this.props.path
+    };
   }
 
-  startChat = async() => {
+  startChat = async () => {
     try {
-      await database.goOnline()
+      await database.goOnline();
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   componentWillMount() {
-    this.startChat()
+    this.startChat();
   }
   componentDidMount() {
+    console.log(this.props.path);
     database
-      .ref(`${this.props.path}`)
-      .on('child_added', x => this.updateState(x.val()))
+      .ref(`allChat/chat${this.props.path}`)
+      .on("child_added", x => this.updateState(x.val()));
   }
 
   updateState = data => {
     this.setState({
-      chat: this
-        .state
-        .chat
-        .concat([data])
-    })
-  }
+      chat: this.state.chat.concat([data])
+    });
+  };
 
   render() {
     return (
-
       <Grid className="App">
         <Row>
           <Col sm={11} smOffset={1}>
-            <Messages chat={this.state.chat}/>
+            <Messages chat={this.state.chat} />
           </Col>
         </Row>
         <Row>
           <Col sm={6} smOffset={3}>
-
-            <ChatInput user={avocat} path={this.props.path}/>
+            <ChatInput user={avocat} path={this.props.path} />
           </Col>
         </Row>
-
       </Grid>
-
-    )
+    );
   }
 }
